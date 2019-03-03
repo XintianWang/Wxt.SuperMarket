ca42dae52b2f64eec57c62634b4875380ff38726
xintian.wang1973@gmail.com <xintian.wang1973@gmail.com>
2019-03-03 11:56:08 -0500

    add product class

--- Wxt.OnlineSuperMarket.Data.Repositories/InMemorySuperMarketRepository.cs
+++ Wxt.OnlineSuperMarket.Data.Repositories/InMemorySuperMarketRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wxt.OnlineSuperMarket.Data.Entities;
 
 namespace Wxt.OnlineSuperMarket.Data.Repositories
 {
     public class InMemorySuperMarketRepository
     {
+        private static readonly List<Product> _products = new List<Product>();
     }
 }
