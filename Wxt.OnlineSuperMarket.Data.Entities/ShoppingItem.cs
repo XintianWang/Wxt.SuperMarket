ae24f2529b6c2750644bbc065064d1517b1a14ea
xintian.wang1973@gmail.com <xintian.wang1973@gmail.com>
2019-03-03 12:46:04 -0500

    modify SupermarketRepository and override toString of Product, ShoppingItem, and Receipt

--- Wxt.OnlineSuperMarket.Data.Entities/ShoppingItem.cs
+++ Wxt.OnlineSuperMarket.Data.Entities/ShoppingItem.cs
@@ -3,8 +3,14 @@
     public class ShoppingItem
     {
         public int ProductId { get; set; }
+        public string ProductName { get; set; }
         public decimal Price { get; set; }
         public int Count { get; set; }
         public decimal PriceSum => Price * Count;
+
+        public override string ToString()
+        {
+            return $"{ProductId} {ProductName} {Count} @{Price} {PriceSum}";
+        }
     }
 }
--- Wxt.OnlineSuperMarket.Data.Repositories/InMemorySuperMarketRepository.cs
+++ Wxt.OnlineSuperMarket.Data.Repositories/InMemorySuperMarketRepository.cs
@@ -10,5 +10,7 @@ namespace Wxt.OnlineSuperMarket.Data.Repositories
     public class InMemorySuperMarketRepository
     {
         private static readonly List<Product> _products = new List<Product>();
+        private static readonly List<ProductItem> _stock = new List<ProductItem>();
+        private static readonly List<Receipt> _receipts = new List<Receipt>();
     }
 }
