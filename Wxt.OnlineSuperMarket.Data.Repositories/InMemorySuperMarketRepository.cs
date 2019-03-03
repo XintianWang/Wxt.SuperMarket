8d31e98e2aaf6e6295ac1b9e91366bd43275def2
xintian.wang23@gmail.com <xintian.wang23@gmail.com>
2019-03-03 13:08:46 -0500

    Add initialization repositories

--- Wxt.OnlineSuperMarket.Data.Repositories/InMemorySuperMarketRepository.cs
+++ Wxt.OnlineSuperMarket.Data.Repositories/InMemorySuperMarketRepository.cs
@@ -9,8 +9,18 @@ namespace Wxt.OnlineSuperMarket.Data.Repositories
 {
     public class InMemorySuperMarketRepository
     {
-        private static readonly List<Product> _products = new List<Product>();
-        private static readonly List<ProductItem> _stock = new List<ProductItem>();
+        private static readonly List<Product> _products = new List<Product>()
+        {
+            new Product{ Id = 1, Name = "banana", Category = Category.Grocery, Description = "Banana from Mexico", Price = 1.67m},
+            new Product{ Id = 2, Name = "apple", Category = Category.Grocery, Description = "Apple from China", Price = 2.67m},
+            new Product{ Id = 3, Name = "TV", Category = Category.Electronic, Description = "Sony 65\"", Price = 1600.59m}
+        };
+        private static readonly List<ProductItem> _stock = new List<ProductItem>()
+        {
+            new ProductItem {ProductId = 1, Count = 100},
+            new ProductItem {ProductId = 2, Count = 200},
+            new ProductItem {ProductId = 3, Count = 300},
+        };
         private static readonly List<Receipt> _receipts = new List<Receipt>();
     }
 }
